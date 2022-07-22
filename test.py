3c1b56115c47ffec152afb54f35368aa88d4a04e
hasanmahani08 <hasanmahani08@gmail.com>
2022-07-22 20:40:39 +0430

    test file enhanced

--- test.py
+++ test.py
@@ -5,13 +5,9 @@ game = GameBoard(need_print=True, info=GameInfo(blocks=[(1, 1), (2, 2), (4, 1), 
                                                 cheese=[(0, 2), (2, 3), (3, 2), (4, 2), (5, 3)]))
 #    print("New Game")
 state = game.calcStateNum()
-gameOver = False
-score = 0
-while not gameOver:
-    print("state is:", state)
-    action = policy[state[0], state[1], state[2], :]
-    tmp = game.move(action=action)
-    new_state, reward = game.move(action=action)
-    print("reward", reward)
-    gameOver = game.game_ended
-    state = new_state
+action = policy[state[0], state[1], state[2], :]
+tmp = game.move(action=action)
+new_state, reward = game.move(action=action)
+print("reward", reward)
+state = new_state
+game.play()
